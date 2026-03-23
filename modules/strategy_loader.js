ff274b9903cca43f28ab36028202a7c6357529f1
AlexanderMcQueen-byte <alexandermcqueen136@gmail.com>
2026-03-23 15:53:03 +0000

    cleanup: remove root-level duplicate of BTC trade/modules/strategy_loader.js

--- modules/strategy_loader.js
+++
@@ -1,8 +0,0 @@
-// Dynamically loads strategy plugins
-import path from 'path';
-
-export async function loadStrategy(strategyName) {
-    const strategyPath = path.resolve('modules', `${strategyName}.js`);
-    const module = await import(strategyPath);
-    return module.default;
-}
